9b417f30915215ee33fea03af71c2cff9c962026
untoreh <bassobassista@gmail.com>
2018-11-21 07:27:06 +0100

    added sonarr scripts

---
+++ scripts/sonarr/sonarr_list_episodeFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apikey=4f3a98b538594bd8a58f162fb4cce2b5
+host=localhost:8989
+sleep=600
+
+series=$(curl -s -X GET "$host/api/Series?apikey=$apikey" | jq -r '.[] | "\(.title) |-| \(.id)"')
+
+IFS=$'\n'
+for s in $series; do
+    title=${s/ |-|*}
+    id=${s/*|-| }
+    curl -s -X GET "$host/api/EpisodeFile/?seriesId=${id}&apikey=$apikey"
+done
+
