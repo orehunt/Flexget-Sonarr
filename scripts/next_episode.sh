a1b43dbd84331fe912d5187215d67d409a84cc5b
untoreh <bassobassista@gmail.com>
2019-03-18 14:56:01 +0100

    added caching to next episode script

--- scripts/next_episode.sh
+++ scripts/next_episode.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+cache=/tmp/episode_next_cache
+
 # netcat http server
-while :; do 
+while :; do
     series=$(nc -q1 -l -p 8082)
     echo got $series
-    flexget execute --now --task episode_next_trakt 2>/dev/null | grep -m 1 -i -o -P -m 1 ".*$series.*\|\-\| \KS[0-9]*E[0-9]*" | nc -q1 localhost 8082
+    [ -z "$(find "$cache" -mtime +1 -print)" ] || flexget execute --now --task episode_next_trakt 1>$cache
+    grep -m 1 -i -o -P -m 1 ".*$series.*\|\-\| \KS[0-9]*E[0-9]*" "$cache" | nc -q1 localhost 8082
     sleep 1
 done
